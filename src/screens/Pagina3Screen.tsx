57b4dae014de64b6fced94a89809faf4ca64ce30
Ariel <caminosariel@gmail.com>
2023-02-09 17:10:48 -0300

    install many package and use StackNavigator

---
+++ src/screens/Pagina3Screen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+export const Pagina3Screen = () => {
+  return (
+    <View>
+        <Text>PaginaScreen 3</Text>
+    </View>
+  )
+}
