31ec6254891a09ea5254ef17d93750b6f3d63214
Ariel Caminos <caminosariel@gmail.com>
2023-02-15 20:14:37 -0300

    add icons

--- src/screens/Tab1Sreen.tsx
+++ src/screens/Tab1Sreen.tsx
@@ -1,5 +1,7 @@
 import React, {useEffect} from 'react'
 import { Text, View } from 'react-native'
+import { styles } from '../theme/appTheme'
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export const Tab1Sreen = () => {
     useEffect(() => {
@@ -7,8 +9,13 @@ export const Tab1Sreen = () => {
     }, [])
 
   return (
-    <View>
-        <Text>Tab1Screen</Text>
+    <View style={ styles.globalMargin }>
+        <Text style={ styles.title }>Iconos</Text>
+
+        <Text>
+          <Icon name="airplane-outline" size={40} color="#900" />;
+        </Text>
+
     </View>
   )
 }
